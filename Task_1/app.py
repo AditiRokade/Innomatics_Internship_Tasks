3d18d83bf8c1091a9d6c8bb20240f8b959e5c498
AditiRokade <aditi.rokade.ajr08@gmail.com>
2023-02-27 08:46:17 +0530

    1

--- Task_1/app.py
+++ Task_1/app.py
@@ -15,13 +15,9 @@ def load_lottieurl(url):
     return r.json()
 
 
-# Use local CSS
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-
-local_css("https://github.com/AditiRokade/Innomatics_Internship_Tasks/blob/master/Task_1/style/style.css")
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 # ---- LOAD ASSETS ----
 lottie = load_lottieurl("https://assets5.lottiefiles.com/packages/lf20_sb5rlinb.json")
